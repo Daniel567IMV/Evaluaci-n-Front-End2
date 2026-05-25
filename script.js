b3e0f07fab48884ac1abc7399e88d1db4d876ce3
Daniel <danielimv567@gmail.com>
2026-05-25 14:41:56 -0400

    Parte 3: Implementación del Requerimiento numero 1 en SCRIPT.JS

---
+++ script.js
@@ -0,0 +1,53 @@
+let colaboradores = [];
+
+const formulario = document.getElementById('formularioRegistro');
+const inputNombre = document.getElementById('nombre');
+const inputApellido = document.getElementById('apellido');
+const inputCargo = document.getElementById('cargo');
+const inputEmail = document.getElementById('email');
+const mensajeError = document.getElementById('mensajeError');
+const cuerpoTabla = document.getElementById('cuerpoTabla');
+const buscador = document.getElementById('buscador');
+//Requerimiento 1: Validación y regsistro//
+
+//funcion para validar que el correo termine en @empresa.cl//
+function validarCorreo(correo) {
+    return correo.toLowerCase().endsWith('@empresa.cl');
+}
+
+//evento que se dispara al enviar el formulario//
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    //obtener valores de los campos y quitar espacios en blanco al inicio y al final//
+    const nombre = inputNombre.value.trim();
+    const apellido = inputApellido.value.trim();
+    const cargo = inputCargo.value.trim();
+    const email = inputEmail.value.trim();
+    //validar que los campos no esten vacios//
+    if(nombre === '' || apellido === '' || cargo === '' || email === ''){
+        mensajeError.textContent ='Todos los campos son obligatorios.';
+        return;
+}
+//validar el formato del correo corporativo//
+if(!validarCorreo(email)){
+    mensajeError.textContent = 'El correo debe terminar en @empresa.cl';
+    return;
+}
+//si todo esta correcto, se limpia el mensaje de error//
+mensajeError.textContent = '';
+//crear un objeto literal para el nuevo colaborador//
+//se usa el date.now() para generar un id unico el cual nos sirve para eliminarlo despues//
+const nuevoColaborador = {
+    id: Date.now(),
+    nombre: nombre,
+    apellido: apellido,
+    cargo: cargo,
+    email: email
+};
+//agregar el nuevo colaborador al arreglo de colaboradores//
+colaboradores.push(nuevoColaborador);
+//actualizar la tabla y resetear el formulario//
+renderizarTabla(colaboradores);
+formulario.reset();
+});
+//Requerimiento 2: Listado Dinámico//
