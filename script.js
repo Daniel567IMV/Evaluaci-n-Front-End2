19e951cdd8c65e67480cbe8d6077b5279ec9a277
Daniel <danielimv567@gmail.com>
2026-05-26 22:21:55 -0400

    Parte 4: Se crea en base el requerimiento numero 2: El listado dinamico renderizado

--- script.js
+++ script.js
@@ -51,3 +51,26 @@ renderizarTabla(colaboradores);
 formulario.reset();
 });
 //Requerimiento 2: Listado Dinámico//
+ //función pR renderizar la tabla, recibe un arreglo(puede ser el completo o el filtrado) para mostrarlo en el HTML//
+ function renderizarTabla(listaColaboradores){
+    //se limpia el contenido anterior de la tabla//
+    cuerpoTabla.innerHTML = '';
+    //iteramos sobre el arreglo de colaboradores para crear las filas//
+    listaColaboradores.forEach(function(colaborador){
+    //se crea una fila//
+    const fila = document.createElement('tr');
+    //se inserta las celdas(td) con la informacion del objeto template literals, en la ultima celda agrego el boton de eliminar, pasandole la Id del //
+    fila.innerHTML = `
+        <td>${colaborador.nombre}</td>
+        <td>${colaborador.apellido}</td>
+        <td>${colaborador.cargo}</td>
+        <td>${colaborador.email}</td>
+        <td>
+            <button class="btn-eliminar" onclick="eliminarColaborador(${colaborador.id})">Eliminar</button>  
+        </td>
+    `;
+    
+    //se agrega la fila al cuerpo de la tabla//
+    cuerpoTabla.appendChild(fila);
+    });
+}
